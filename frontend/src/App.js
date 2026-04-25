d8f447c3fda49e3483363ab897e089ac4c06bee0
unknown <you@example.comedmforyoulite@gmail.com>
2026-04-25 16:48:05 +0530

    fix apo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,6 +17,8 @@ function App() {
   const [token, setToken] = useState(localStorage.getItem("token"));
   const [user, setUser] = useState(null);
   const [showSidebar, setShowSidebar] = useState(false);
+  const API = "https://panel-y8tg.onrender.com"; // ✅ ADD THIS
+
   const [toast, setToast] = useState({
     show: false,
     message: "",
